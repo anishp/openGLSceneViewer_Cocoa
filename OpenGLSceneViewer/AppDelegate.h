e463289a5ebd93835d8b960651efa401846c973b
Anish Pednekar <pednekar.anish@gmail.com>
2014-07-14 23:01:19 +0530

    Initial Commit

---
+++ OpenGLSceneViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLSceneViewer
+//
+//  Created by Anish Pednekar on 7/14/14.
+//  Copyright (c) 2014 Anish Pednekar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ OpenGLSceneViewer/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  OpenGLSceneViewer
+//
+//  Created by Anish Pednekar on 7/14/14.
+//  Copyright (c) 2014 Anish Pednekar. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
